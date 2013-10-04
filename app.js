4459491750d2d85b03f09e998f4a79464cd0f0bf
Tim Fogarty <timothy.fogo@gmail.com>
2013-10-04 18:43:41 +0100

    Move views

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ client  = arDrone.createClient();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(app.router);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 
 app.get('/', routes.index);
 
 app.listen(3000, function(){
-    console.log("Server started on port %d", app.address().port);
+    console.log("Server started on port 3000");
 });
