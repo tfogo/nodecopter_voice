fbb177d804847c3949fee66690bdfc56956950b1
Tim Fogarty <timothy.fogo@gmail.com>
2013-11-23 18:33:59 +0000

    Adds routes closure

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+app.set('client', client);
 
 app.get('/', routes.index);
-app.get('/takeoff', routes.takeoff);
+app.get('/takeoff', routes.takeoff(client));
+app.get("/land", routes.land(client));
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
--- routes.js
+++ routes.js
@@ -1,13 +1,18 @@
 exports.index = function(req, res){
     res.render('index', {'title':'HI THERE'});
-}
+};
 
-exports.takeoff = function(req, res){
-    console.log("Takeoff!");
-    client.takeoff();
-}
+exports.land = function(client){
+    return function(req, res) {
+        console.log("Land!");
+	client.land();
+    };
+};
 
-exports.land = function(req, res){
-    console.log("Land!");
-    client.land();
-}
+exports.takeoff =  function(client){
+    return function(req, res) {
+        console.log("Takeoff!");
+	client.takeoff();
+    };
+};
+
